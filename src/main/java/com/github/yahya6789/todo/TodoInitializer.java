fb888bb9873da0c49d1c1e539127ab4855853e80
Yahya <yahya6789@gmail.com>
2025-07-07 09:47:27 +0700

    feat: add initial Todo data via TodoInitializer

---
+++ src/main/java/com/github/yahya6789/todo/TodoInitializer.java
@@ -0,0 +1,25 @@
+package com.github.yahya6789.todo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.github.yahya6789.todo.model.Todo;
+import com.github.yahya6789.todo.repository.TodoRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class TodoInitializer implements CommandLineRunner {
+
+    private final TodoRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (repository.count() == 0) {
+            repository.save(new Todo("Learn Spring Boot"));
+            repository.save(new Todo("Build a Todo API"));
+            repository.save(new Todo("Explore HATEOAS"));
+        }
+    }
+}
