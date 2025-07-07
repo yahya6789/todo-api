7d241b6c197a07aa70663e9bcd3121214d83939a
Yahya <yahya6789@gmail.com>
2025-07-07 14:20:26 +0700

    feat: seed 55 initial todos on startup if repository is empty

--- src/main/java/com/github/yahya6789/todo/TodoInitializer.java
+++ src/main/java/com/github/yahya6789/todo/TodoInitializer.java
@@ -1,5 +1,7 @@
 package com.github.yahya6789.todo;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,9 +19,9 @@ public class TodoInitializer implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         if (repository.count() == 0) {
-            repository.save(new Todo("Learn Spring Boot"));
-            repository.save(new Todo("Build a Todo API", true));
-            repository.save(new Todo("Explore HATEOAS"));
+            for (int i = 0; i < 55; i++) {
+                repository.save(new Todo("Todo " + i, ThreadLocalRandom.current().nextBoolean()));
+            }
         }
     }
 }
