c058065d3b8f3459a44f7baf35827c7c07cf4547
Yahya <yahya6789@gmail.com>
2025-07-08 09:23:56 +0700

    chore: move todo-service module and remove nested git repo

--- src/main/java/com/github/yahya6789/todo/repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.github.yahya6789.todo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.github.yahya6789.todo.model.Todo;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
