ee44c83793468cffc86dacc572857af87b3d085b
Yahya <yahya6789@gmail.com>
2025-07-09 12:16:52 +0700

    chore: remote unused imports

--- todo-service/src/main/java/com/github/yahya6789/todo/model/Todo.java
+++ todo-service/src/main/java/com/github/yahya6789/todo/model/Todo.java
@@ -3,14 +3,9 @@ package com.github.yahya6789.todo.model;
 import com.github.yahya6789.common.model.AuditableEntity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AccessLevel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Setter;
 
 @Entity
 @EqualsAndHashCode(callSuper = true)
